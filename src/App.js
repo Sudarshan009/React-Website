96703ea93fe6d5b3a9fcf97ba21db913d625694e
Sudarshan009 <sudarshan.mali09@gmail.com>
2020-09-27 01:03:34 +0530

    navbar fixed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Portfolio from "./components/Portfolio";
 import thumbimg from "./assets/img/portfolio/warrior.jpg";
+import Navbar from "./components/Navbar";
 
 function App() {
   const portfolioLinks = [
@@ -39,56 +40,7 @@ function App() {
   return (
     <div className="App">
       <body id="page-top">
-        <nav
-          className="navbar navbar-expand-lg navbar-dark fixed-top"
-          id="mainNav"
-        >
-          <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">
-              <img src="/src/assets/img/logos/logonew.jpg" alt="" />
-            </a>
-            <button
-              className="navbar-toggler navbar-toggler-right"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarResponsive"
-              aria-controls="navbarResponsive"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              Menu<i className="fas fa-bars ml-1"></i>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav text-uppercase ml-auto">
-                <li className="nav-item">
-                  <a className="nav-link js-scroll-trigger" href="#services">
-                    Services
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link js-scroll-trigger" href="#portfolio">
-                    Portfolio
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link js-scroll-trigger" href="#about">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link js-scroll-trigger" href="#team">
-                    Team
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link js-scroll-trigger" href="#contact">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navbar></Navbar>
 
         <header className="masthead">
           <div className="container">
